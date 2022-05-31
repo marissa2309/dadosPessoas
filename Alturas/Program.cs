d9f8b6b3c470738b93eda7cc4214a34fbe66926a
marissa2309 <102029727+marissa2309@users.noreply.github.com>
2022-05-31 10:06:29 -0300

    Add files via upload

---
+++ Alturas/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace alturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, i, cont;
+            double soma, media, porcentagem;
+
+            Console.WriteLine("Quantas pessoas serão digitadas");
+            N = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[N];
+            double[] idades = new double[N];
+            double[] alturas = new double[N];
+
+            for (i = 0; i < N; i++)
+            {
+                Console.WriteLine("Dados da " + (i+1) + " a pessoa");
+                Console.WriteLine("Nome: ");
+                nomes[i] = Console.ReadLine();
+                Console.WriteLine("Idade: ");
+                idades[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine("Altura: ");
+                alturas[i] = double.Parse(Console.ReadLine());  
+            }
+
+            soma = 0;
+
+            for(i = 0; i < N; i++) 
+            {
+                soma += alturas[i];
+            }
+
+            media = soma / N;
+            Console.WriteLine();
+            Console.WriteLine("Altura media: " + media);
+
+            cont = 0;  
+
+            for(i= 0; i<N; i++)
+            {
+                if (idades[i] < 16)
+                {
+                    cont += 1;
+                }
+            }
+
+            porcentagem = cont * 100 / N;
+            Console.WriteLine("Pessoas com menos de 16 anos: " + porcentagem + " % ");
+
+            for (i = 0; i < N;i ++)
+            {
+                if (idades[i] < 16)
+                {
+                    Console.WriteLine(nomes[i]);
+                }  
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+class comerciante
+{
+    static void Main(string[] args)
+    {
+        int n, abaixo, entre, acima;
+        float totalCompra, totalVenda, totalLucro, lucro, percentualLucro; Console.WriteLine("Quantos produtos?");
+        n = int.Parse(Console.ReadLine()); float[] precosCompra = new float[n];
+        float[] precosVenda = new float[n];
+        string[] nomes = new string[n]; for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Produto " + (i + 1) + ":");
+            Console.WriteLine("Nome: ");
+            nomes[i] = Console.ReadLine();
+            Console.WriteLine("Preço da compra: ");
+            precosCompra[i] = float.Parse(Console.ReadLine());
+            Console.WriteLine("Preço da venda: ");
+            precosVenda[i] = float.Parse(Console.ReadLine());
+        }
+        abaixo = 0;
+        entre = 0;
+        acima = 0; for (int i = 0; i < n; i++)
+        {
+            lucro = precosVenda[i] - precosCompra[i];
+            percentualLucro = (lucro * 100) / precosCompra[i]; if (percentualLucro < 10)
+            {
+                abaixo++;
+            }
+            else
+            {
+                if (percentualLucro <= 20)
+                {
+                    entre++;
+                }
+                else
+                {
+                    acima++;
+                }
+            }
+        }
+        totalCompra = 0;
+        totalVenda = 0; for (int i = 0; i < n; i++)
+        {
+            totalCompra += precosCompra[i];
+            totalVenda += precosVenda[i];
+        }
+        totalLucro = totalVenda - totalCompra; Console.WriteLine("RELATÓRIO: ");
+        Console.WriteLine("Lucro abaixo de 10%: " + abaixo);
+        Console.WriteLine("Lucro entre 10% e 20%: " + entre);
+        Console.WriteLine("Lucro acima de 20%: " + acima);
+        Console.WriteLine("Valor total de compra: " + Math.Round(totalCompra, 2));
+        Console.WriteLine("Valor total de venda: " + Math.Round(totalVenda, 2));
+        Console.WriteLine("Lucro total: " + Math.Round(totalLucro, 2));
+    }
+}
+
+
